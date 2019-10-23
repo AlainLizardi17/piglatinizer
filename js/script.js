2b3742f589919886607a864b78c655c2828ea86d
AlainLizardi17 <alizardi.v@rrghst.org>
2019-10-23 22:15:30 +0000

    working on progress to change words

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function
+    let vowels;
+        vowels = "a, e, i, o, u";
 
 
 
-  
-  
-  
-  
+
+
+
 });
